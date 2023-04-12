2c59105d6b26e9a33bc926d96d1bdb48060a83eb
Mregojos <86085713+Mregojos@users.noreply.github.com>
2023-04-12 17:20:57 +0800

    Update scripts.sh

--- app/scripts.sh
+++ app/scripts.sh
@@ -1,1 +1,8 @@
+# To build and run stremlit 
+docker build -t streamlit-app .
+docker run --name streamlit-app -d -p 8501:80 -v $(pwd):/app streamlit-app
+
+# docker ps
+# docker rm -f streamlit-app
+
 
